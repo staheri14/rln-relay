877844d0e681a2f2d2de810eadc472938bf8a27d
Sanaz <staheri14@ku.edu.tr>
2022-04-07 15:05:15 -0700

    cleans up the migration

--- migrations/3_rln_migrations.js
+++ migrations/3_rln_migrations.js
@@ -1,16 +1,3 @@
-const PoseidonHasher = artifacts.require("./crypto/PoseidonHasher");
-
-// var address
-// module.exports = function(deployer) {
-//   deployer.deploy(PoseidonHasher)
-
-//     // Option 2) Console log the address:
-//     .then(() => address = PoseidonHasher.address)
-
-//     // Option 3) Retrieve the contract instance and get the address from that:
-//     // .then(() => SimpleStorage.deployed())
-//     // .then(_instance => console.log(_instance.address));
-// };
 var deposit = web3.utils.toBN(5);
 var depth = web3.utils.toBN(40);
 const RLN = artifacts.require("RLN");
